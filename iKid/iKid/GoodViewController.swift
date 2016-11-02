e3e70baf1b46560325dfcfb921a836073b16c257
luludust <elizabethduong96@gmail.com>
2016-11-01 22:38:08 -0700

    Initial views

---
+++ iKid/iKid/GoodViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GoodViewController.swift
+//  iKid
+//
+//  Created by iGuest on 11/1/16.
+//  Copyright © 2016 Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class GoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
