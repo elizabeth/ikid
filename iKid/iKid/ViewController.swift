34f9394f183ec5cd648eb657a267c283956ca5fb
luludust <elizabethduong96@gmail.com>
2016-11-02 17:16:28 -0700

    code

--- iKid/iKid/ViewController.swift
+++ iKid/iKid/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet var joke: UIView!
     @IBOutlet var answer: UIView!
+    var type = ""
+    var knockJokes = ["knock knock", "who's there?", "aaa", "aaa who?", "aafiefojk"]
+    var knockCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
+        
+        type = title!
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,12 +48,43 @@ class ViewController: UIViewController {
     @IBAction func nextButton(_ sender: AnyObject) {
 //        answer.isHidden = false
 //        joke.isHidden = true
-        
-        UIView.transition(from: joke, to: answer, duration: 1.0, options: .transitionFlipFromLeft, completion: { (finished: Bool) -> Void in
-            sleep(1)
-            UIView.transition(from: self.answer, to: self.joke, duration: 1.0, options: .transitionFlipFromRight, completion: nil)
+        print(type)
+        if type != "Dad" {
+            UIView.transition(from: joke, to: answer, duration: 1.0, options: .transitionFlipFromLeft, completion: { (finished: Bool) -> Void in
+                usleep(750000)
+                UIView.transition(from: self.answer, to: self.joke, duration: 1.0, options: .transitionFlipFromRight, completion: nil)
+            })
+        } else {
+            knockCount += 1
+            
+            switch knockCount {
+            case 1:
+                UIView.animate(withDuration: 0.7, delay: 1.0, options: .transitionFlipFromLeft, animations: {
+                print(self.joke)
+                }, completion: nil)
+            case 2:
+                UIView.animate(withDuration: 0.7, delay: 1.0, options: .transitionFlipFromLeft, animations: {
+                    //do shit
+                    }, completion: nil)
+            case 3:
+                UIView.animate(withDuration: 0.7, delay: 1.0, options: .transitionFlipFromLeft, animations: {
+                    //do shit
+                    }, completion: nil)
+            case 4:
+                UIView.animate(withDuration: 0.7, delay: 1.0, options: .transitionFlipFromLeft, animations: {
+                    //do shit
+                    }, completion: nil)
+//                UIView.animate(withDuration: 0.7, delay: 1.0, options: .transitionFlipFromLeft, animations: {
+//                    //do shit
+//                    }, completion: (_ : Bool) -> Void in {
+//                        UIView.animate(withDuration: 0.7, delay: 1.0, options: .transitionFlipFromRight, animations: {
+//                            //do shit
+//                            }, completion: nil)
+//                    })
+            default:
+                break
             }
-        )
+        }
     }
 }
 
